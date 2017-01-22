a2e0f5a150c811872edd8e56ca8f71610bb798aa
gogoge <tsung.ying.lu@gmail.com>
2017-01-22 07:20:22 +0000

    add two domain, and remove test-cert argument,  but haproxy.cfg still cannot eat env var

--- haproxy/init.sh
+++ haproxy/init.sh
@@ -1,6 +1,8 @@
 if [ ! -f "/etc/letsencrypt/keys/0000_key-certbot.pem" ]; then
   echo "starting certbot"
-  certbot certonly --standalone --redirect --test-cert --non-interactive --agree-tos --email $EMAIL1 -d $DOMAIN1
+  # certbot certonly --standalone --redirect --test-cert --non-interactive --agree-tos --email $EMAIL1 -d $DOMAIN1 -d $DOMAIN2 -d $DOMAIN3
+  # test cert on above, offical on below
+  certbot certonly --standalone --redirect --non-interactive --agree-tos --email $EMAIL1 -d $DOMAIN1 -d $DOMAIN2 -d $DOMAIN3
   certbot renew --dry-run
   while [ ! -f /etc/letsencrypt/live/$DOMAIN1/privkey.pem ]
   do
--- haproxy/run.sh
+++ haproxy/run.sh
@@ -1,6 +1,8 @@
 docker run -d \
   -p 80:80 -p 443:443 -p 8888:8888 \
   -e DOMAIN1=$DOMAIN1 \
+  -e DOMAIN2=$DOMAIN2 \
+  -e DOMAIN3=$DOMAIN3 \
   -e EMAIL1=$EMAIL1 \
   --restart=always \
   --name haproxy \
